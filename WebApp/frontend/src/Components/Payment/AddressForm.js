2a082108e8d8ab3ddf3895b586c9a542b7e7ed5e
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-05-05 21:51:53 +0200

    -reserwation from wip

--- WebApp/frontend/src/Components/Payment/AddressForm.js
+++ WebApp/frontend/src/Components/Payment/AddressForm.js
@@ -11,14 +11,14 @@ const AddressForm = ({ setAddressData }) =>{
     }
 
     return (
-        <div className = "AddressForm">
-            <h1 className = "AddressFormTitle">Adres</h1>
+        <div className = "Form">
+            <h1 className = "FormTitle">Adres</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
 
-                <div className = "AddressFormRow">
-                    <h3 className = "AddressFormName">E-Mail</h3>
+                <div className = "FormRow">
+                    <h3 className = "FormName">E-Mail</h3>
 
-                    <input className = "AddressFormElement" type = "text" placeholder = 'Email' { ...register("email",{
+                    <input className = "FormElement" type = "text" placeholder = 'Email' { ...register("email",{
                         required: "Nieprawidłowy e-mail",
                         pattern: {
                             value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
@@ -28,18 +28,18 @@ const AddressForm = ({ setAddressData }) =>{
 
                 </div>
 
-                <div className = "AddressFormRow">
-                    <h3 className = "AddressFormName">Miejscowość</h3>
+                <div className = "FormRow">
+                    <h3 className = "FormName">Miejscowość</h3>
 
-                    <input className = "AddressFormElement" type = "text" placeholder = 'Miejscowość' { ...register("town", {
+                    <input className = "FormElement" type = "text" placeholder = 'Miejscowość' { ...register("town", {
                         required: "Nieprawidłowa miejsowość"
                         }) } />
                 </div>
 
-                <div className = "AddressFormRow">
-                    <h3 className = "AddressFormName">Kod pocztowy</h3>
+                <div className = "FormRow">
+                    <h3 className = "FormName">Kod pocztowy</h3>
 
-                    <input className = "AddressFormElement" type = "text" placeholder = 'Kod pocztowy' { ...register("postalCode", {
+                    <input className = "FormElement" type = "text" placeholder = 'Kod pocztowy' { ...register("postalCode", {
                         required: "Nieprawidłowy kod pocztowy",
                         pattern: {
                             value: /^[0-9]{2}-[0-9]{3}$/i,
@@ -47,66 +47,66 @@ const AddressForm = ({ setAddressData }) =>{
                         }})} />
                 </div>
 
-                <div className = "AddressFormRow">
-                    <h3 className = "AddressFormName">Ulica</h3>
+                <div className = "FormRow">
+                    <h3 className = "FormName">Ulica</h3>
 
-                    <input className = "AddressFormElement" type = "text" placeholder = 'Ulica' { ...register("street", {
+                    <input className = "FormElement" type = "text" placeholder = 'Ulica' { ...register("street", {
                         required: "Nieprawidłowa ulica"
                         }) } />
                 </div>
 
-                <div className = "AddressFormRow">
-                    <h3 className = "AddressFormName">Numer domu</h3>
+                <div className = "FormRow">
+                    <h3 className = "FormName">Numer domu</h3>
 
-                    <input className = "AddressFormElement" type = "number" placeholder = 'Numer domu' { ...register("houseNumber", {
+                    <input className = "FormElement" type = "number" placeholder = 'Numer domu' { ...register("houseNumber", {
                         required: "Nieprawidłowy numer domu"
                         }) } />
                 </div>
 
-                <div className = "AddressFormRow">
-                    <h3 className = "AddressFormName">Numer mieszkania</h3>
-                    <input className = "AddressFormElement" type = "number" placeholder = 'Numer mieszkania' { ...register("apartmentNumber", {
+                <div className = "FormRow">
+                    <h3 className = "FormName">Numer mieszkania</h3>
+                    <input className = "FormElement" type = "number" placeholder = 'Numer mieszkania' { ...register("apartmentNumber", {
                         }) } />
                 </div>
 
 
-                <ErrorMessage className = "AddressFormError"
+                <ErrorMessage className = "FormError"
                     errors={errors}
                     name="email"
                     render={({message}) => <h4>{message}</h4>}
                 />
 
-                <ErrorMessage  className = "AddressFormElement"
+                <ErrorMessage  className = "FormElement"
                     errors={errors}
                     name="town"
                     render={({message}) => <h4>{message}</h4>}
                 />
 
-                <ErrorMessage  className = "AddressFormElement"
+                <ErrorMessage  className = "FormElement"
                     errors={errors}
                     name="postalCode"
                     render={({message}) => <h4>{message}</h4>}
                 />
 
-                <ErrorMessage  className = "AddressFormElement"
+                <ErrorMessage  className = "FormElement"
                     errors={errors}
                     name="street"
                     render={({message}) => <h4>{message}</h4>}
                 />
 
-                <ErrorMessage  className = "AddressFormElement"
+                <ErrorMessage  className = "FormElement"
                     errors={errors}
                     name="houseNumber"
                     render={({message}) => <h4>{message}</h4>}
                 />
 
-                <ErrorMessage  className = "AddressFormElement"
+                <ErrorMessage  className = "FormElement"
                     errors={errors}
                     name="apartmentNumber"
                     render={({message}) => <h4>{message}</h4>}
                 />
 
-                <input className = "AddressFormSubmit" type = "submit" />
+                <input className = "FormSubmit" type = "submit" />
             </form>
         </div>
     )
--- WebApp/frontend/src/Components/Reservation/ReservationPage.js
+++ WebApp/frontend/src/Components/Reservation/ReservationPage.js
@@ -1,32 +1,140 @@
 import React, {useState} from 'react'
 import { Controller, useForm } from 'react-hook-form'
-import DatePicker from 'react-datepicker'
+import { ErrorMessage } from '@hookform/error-message'
+import TimePicker from 'react-time-picker'
+import DatePicker, { getDefaultLocale } from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
 const ReservationPage = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date())
-  const { control, register, handleSubmit } = useForm()
+  const {control, register, handleSubmit, formState: {errors} } = useForm();
+  const [selectedDate, setSelectedDate] = useState(null)
+
+  const onSubmit = (data) =>
+  {
+      //setAddressData(data)
+  }
+
+
 
   return (
     <div className = "ReservationPageContent">
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-       <form>
-       <Controller
+      <div className = "Form">
+      <h1 className = "FormTitle">Rezerwacja</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <div className = "FormRow">
+        <h3 className = "FormName">Data</h3>
+
+        <Controller
         control={control}
-        name='date-input'
+        name='date'
+        required = "Data jest wymagana"
         render={({ field }) => (
-          <DatePicker 
+          <DatePicker className = "FormElementDate"
+            popperProps={{strategy: 'fixed'}}
+            placeholderText='Select date'
+            onChange={(date) => {field.onChange(date); setSelectedDate(date) } }
+            selected={field.value}
+            filterDate={date => date.getDay() != 0 }
+            />
+         )}
+        />
+      </div>
+
+      <div className = "FormRow">
+        <h3 className = "FormName">Godzina</h3>
+
+        <Controller
+        control={control}
+        name='time'
+        required = "czas jest wymagany"
+        render={({ field }) => (
+          <TimePicker className = "FormElementTime"
             popperProps={{strategy: 'fixed'}}
             placeholderText='Select date'
             onChange={(date) => field.onChange(date)}
             selected={field.value}
+            disableClock={true}
+            maxDetail = "hour"
             />
          )}
         />
+      </div>
+
+      <div className = "FormRow">
+          <h3 className = "FormName">Nr Telefonu</h3>
+
+          <input className = "FormElement" type = "text" placeholder = 'Nr Telefonu' { ...register("phone",{
+              required: "Nieprawidłowy nr telefonu",
+              pattern: {
+                  value: /^[0-9]{9}$/,
+                  message: "Nieprawidłowy nr telefonu"
+              }
+              }) } />
+      </div>
+
+      <div className = "FormRow">
+          <h3 className = "FormName">Liczba Osób</h3>
+
+          <input className = "FormElement" type = "number" placeholder = 'Liczba Osób' { ...register("numberOfPeople",{
+              required: "Nieprawidłowa liczba osób",
+              validate: {
+                positive: v => {
+                  if(v < 1)
+                  {
+                    return "Nieprawidłowa liczba osób"
+                  }
+                  return true
+                 }
+              }
+              }) } />
+      </div>
+
+      <div className = "FormRow">
+          <h3 className = "FormName">E-Mail</h3>
+
+          <input className = "FormElement" type = "text" placeholder = 'Email' { ...register("email",{
+              required: "Nieprawidłowy e-mail",
+              pattern: {
+                  value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
+                  message: "Nieprawidłowy e-mail"
+              }
+              }) } />
+      </div>
+
+      <ErrorMessage className = "FormError"
+          errors={errors}
+          name="date"
+          render={({message}) => <h4>{message}</h4>}
+      />
+
+      <ErrorMessage className = "FormError"
+          errors={errors}
+          name="time"
+          render={({message}) => <h4>{message}</h4>}
+      />
+
+      <ErrorMessage className = "FormError"
+          errors={errors}
+          name="phone"
+          render={({message}) => <h4>{message}</h4>}
+      />
+
+      <ErrorMessage className = "FormError"
+          errors={errors}
+          name="numberOfPeople"
+          render={({message}) => <h4>{message}</h4>}
+      />
+
+      <ErrorMessage className = "FormError"
+          errors={errors}
+          name="email"
+          render={({message}) => <h4>{message}</h4>}
+      />
+
+      <input className = "FormSubmit" type = "submit" />
       </form>
+      </div>
     </div>
   )
 }
